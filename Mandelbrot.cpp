efb2f040d044e1e42a6ed4e775db548c0a88cd8d
Thomas Anderson <tanderson@caltech.edu>
2017-07-26 12:50:30 -0400

    working mandelbrot

--- Mandelbrot.cpp
+++ Mandelbrot.cpp
@@ -9,54 +9,12 @@
 #include "Mandelcalc.hpp"
 
 int main(int argc, char **argv) {
-/*  // Initialize MPI before Kokkos
   MPI_Init(&argc, &argv);
 
   // Initialize Kokkos
   Kokkos::initialize(argc, argv);
 
-  const int dim = 1000;
-
-  // Allocate our arrays
-  Kokkos::View<double*> a("a", dim);    
-  Kokkos::View<double*> b("b", dim);
-  Kokkos::View<double*> c("c", dim);
-
-  // Create host mirror of C
-  auto  c_mirror = Kokkos::create_mirror_view(c);
-
-  // Initialize a and b
-  Kokkos::parallel_for(dim, KOKKOS_LAMBDA(int i) {
-      a(i) = sin((double)i)*sin((double)i);
-      b(i) = cos((double)i)*cos((double)i);    
-  });
- 
-  // Preform the vector addition
-  Kokkos::parallel_for(dim, KOKKOS_LAMBDA(int i) {
-    c(i) = a(i) + b(i);
-  });
-
-  // Update the mirror
-  deep_copy(c_mirror, c);
-
-  // Verify the results of the host mirror
-  for(int i=0; i<dim; i++) {
-    double eps = abs(1.0 - c_mirror(i));
-    assert(eps <= std::numeric_limits<double>::epsilon());    
-  };
-
-  std::cout<<"Sum Correct\n";
-
-  Kokkos::finalize();
-
-  MPI_Finalize();
-*/
-  MPI_Init(&argc, &argv);
-
-  // Initialize Kokkos
-  Kokkos::initialize(argc, argv);
-
-  int pixelcountx = 4096;
+  int pixelcountx = 128;
   float centerx = -0.75;
   float centery =  0.00;
   float lengthx = 2.75;
@@ -80,12 +38,11 @@ int main(int argc, char **argv) {
   });
   deep_copy(pixels_mirror, pixels);
 
-  std::ofstream image;
-  image.open("mandelbrot.pgm");
+  std::ofstream image("mandelbrot.pgm", std::ios_base::out | std::ios_base::binary | std::ios_base::trunc);
   if (image.is_open()) {
-    image << "P5" << std::endl;
-    image << pixelcountx << " " << pixelcounty << std::endl;
-    image << 255 << std::endl;
+    image << "P5\n";
+    image << pixelcountx << " " << pixelcounty << "\n";
+    image << 255 << "\n";
     for (int j = 0; j < pixelcounty; j++) {
       for (int i = 0; i < pixelcountx; i++) {
         image << pixels_mirror(i, j);
--- Mandelcalc.hpp
+++ Mandelcalc.hpp
@@ -9,16 +9,16 @@ int MandelCalcBW(float x, float y){
   float rad = 0, zx = 0, zy = 0, rad_max = 2.0;
 
   while(rad < rad_max && iter < iter_max){
-    float zx_new = zx*zx +zy*zy + x;
-    zy = 2*zx*zy +y;
+    float zx_new = zx*zx - zy*zy + x;
+    zy = 2*zx*zy + y;
     zx = zx_new;
-    rad = sqrt(zx*zx +zy*zy);
-    iter++;   
+    rad = sqrt(zx*zx + zy*zy);
+    iter++;
   }//end while
 
   int val = 0;
   if (iter < iter_max) {
-    //this pixel is in the mandelbrot set
+    //this pixel is not in the mandelbrot set
     val = 255;
   }
   return(val);
